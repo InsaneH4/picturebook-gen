2927561125d7269dc253b6a372936b3714cdbb21
Yousef Osman <osman.yousef6933@knights.ucf.edu>
2023-09-16 08:26:28 -0400

    commited changes to mainhub and home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,19 +1,19 @@
 // src/pages/Home.tsx
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link for routing
 import './home.css';
 
-const home: React.FC = () => {
+const Home: React.FC = () => {
   return (
     <div className="homepage">
       <div className="content">
         <h1>Welcome to our Website</h1>
         <p>Discover amazing features and services.</p>
-        <button className="sign-in-button">Sign In</button>
+        <Link to="/signup" className="sign-in-button">Sign Up</Link>
       </div>
     </div>
   );
 };
 
-export default home;
-
+export default Home;
 
