c4c5b44573bf645ad8e4b669fa36a6b72fcda87e
animetibs <101466538+animetibs@users.noreply.github.com>
2023-09-16 09:23:59 -0400

    pretty

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,20 +1,21 @@
 // src/pages/Home.tsx
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link for routing
+import { Link } from 'react-router-dom';
 import './styles.css';
-
 
 const Home: React.FC = () => {
   return (
-    <body>
+    <div className="body">
       <div className="content">
-        <h1>Welcome to our Website</h1>
-        <p>Discover amazing features and services.</p>
+        <h1 className="header">Welcome to Our Picturebook</h1>
+        <p className="text">
+          Welcome to our interactive story creation platform! Children actively shape the story through choices, enhanced by gamification elements like rewards and badges. We prioritize education, aligning story generation with learning objectives. AI-powered guidance ensures positive choices, while parents can monitor their children's storytelling. Plus, we offer visual customization for a truly personalized experience. Join us and unleash creativity in a fun and educational way!
+        </p>
         <Link to="/signup" className="button">
           Sign Up
         </Link>
       </div>
-    </body>
+    </div>
   );
 };
 
