bbf31b39ca8ebb923c01daaa2448cb7a45d7b18a
animetibs <101466538+animetibs@users.noreply.github.com>
2023-09-16 08:11:03 -0400

    + page

--- frontend/src/pages/mainhub.tsx
+++ frontend/src/pages/mainhub.tsx
@@ -1,13 +1,20 @@
 // src/pages/MainHub.jsx
 import React from 'react';
+import { Link } from 'react-router-dom'; // Assuming you're using React Router for navigation
 
 function MainHub() {
   return (
     <div>
       <h1>Main Hub</h1>
       <p>This is the main hub of your app.</p>
+      
+      {/* Button to navigate to the child mode page */}
+      <Link to="/childmode">
+        <button>Go to Child Mode</button>
+      </Link>
     </div>
   );
 }
+
 
 export default MainHub;
