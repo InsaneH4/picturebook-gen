cbec3d1367d15a197df4a2533b176ca4a5069a36
Yousef Osman <osman.yousef6933@knights.ucf.edu>
2023-09-16 01:43:12 -0400

    New changes

---
+++ storybooklite/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+// src/pages/Home.tsx
+
+import React from 'react';
+import './pages/Home'
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page content.</p>
+    </div>
+  );
+}
+
+export default Home;
