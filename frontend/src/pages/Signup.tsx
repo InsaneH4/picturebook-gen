5a00e8874e3e6a4798a3b2d0c624106c4395e1b0
animetibs <101466538+animetibs@users.noreply.github.com>
2023-09-16 04:33:44 -0400

    linked main to register

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,11 +1,15 @@
 // src/pages/Signup.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Signup() {
   return (
     <div>
       <h1>Sign Up</h1>
       <p>Sign up for our service here.</p>
+      <Link to="/mainhub" className="register-button">
+          Register
+        </Link>
     </div>
   );
 }
