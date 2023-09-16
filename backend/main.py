01af82a77e5cec4cbe380d755e562669dcfb04c8
Sam H <samuelhussain@live.com>
2023-09-16 08:52:04 -0400

    we ball

--- backend/main.py
+++ backend/main.py
@@ -3,17 +3,29 @@ from flask import Flask
 
 app = Flask(__name__)
 client = replicate.Client(api_token="r8_CLo7yb0uM3cYeHfPs1N7TyKme3Fg4z743YdrG")
-prompt = input("Enter prompt: ")
-result = client.run(
-    "stability-ai/stable-diffusion:27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478",
-    input={"prompt": prompt}
-)
-print(result)
+# prompt = input("Enter prompt: ")
+# result = client.run(
+#    "stability-ai/stable-diffusion:27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478",
+#    input={"prompt": prompt}
+# print(result)
 
 
-@app.route("/pictures")
-def pictures():
-    return {"pictures": result}
+def stableDiff(prompt):
+    return client.run(
+        "stability-ai/stable-diffusion:27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478",
+        input={"prompt": prompt}
+    )[0]
+
+
+@app.route("/imagegen/<prompts>")
+def imagegen(prompts):
+    prompts = prompts.split(",")
+    print("Generating images with prompts: ", prompts)
+    results = []
+    for prompt in prompts:
+        results.append(stableDiff(prompt))
+    print(results)
+    return {"images": results}
 
 
 if __name__ == "__main__":
