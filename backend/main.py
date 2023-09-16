844be4c4914302153dafd5a47864ea8265358ec6
Sam H <samuelhussain@live.com>
2023-09-16 16:06:46 -0400

    more flask

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,4 @@
 import replicate
-import firebase_admin
-from firebase_admin import db
 from flask import Flask
 from flask_cors import CORS
 
@@ -17,13 +15,6 @@ class Story:
         return f"Theme: {self.theme}\nMC Info: {self.mc_info}\nText: {self.text}\nImage Prompt: {self.img_prompt}\nImage URL: {self.img_url}"
 
 
-# firebase setup
-cred_obj = firebase_admin.credentials.Certificate(
-    "storybook-edu-9dd73-firebase-adminsdk-5lvfu-e63ffee7f1.json")
-default_app = firebase_admin.initialize_app(cred_obj, {
-    "databaseURL": "https://storybook-edu-9dd73-default-rtdb.firebaseio.com/"
-})
-
 app = Flask(__name__)
 CORS(app)
 client = replicate.Client(api_token="r8_CLo7yb0uM3cYeHfPs1N7TyKme3Fg4z743YdrG")
@@ -33,8 +24,6 @@ client = replicate.Client(api_token="r8_CLo7yb0uM3cYeHfPs1N7TyKme3Fg4z743YdrG")
 #    input={"prompt": prompt}
 # print(result)
 
-
-ref = db.reference("/Story")
 my_story = Story("no theme", "no mc_info", "no text", "no prompt", "no url")
 
 
@@ -46,13 +35,14 @@ def stable_diff(prompt):
 
 
 # Generates ai image of scene
-@app.route("/imagegen/<prompt>")
-def imagegen(prompt):
-    print("Generating image with prompt: ", prompt)
-    result = stable_diff(prompt)
+@app.route("/imagegen/<my_prompt>")
+def imagegen(my_prompt):
+    print("Generating image with prompt: ", my_prompt)
+    result = stable_diff(my_prompt)
     my_story.img_url = result
+    my_story.img_prompt = my_prompt
     print(result)
-    return {"image_url": result, "prompt": prompt}
+    return {"image_url": my_story.img_url, "prompt": my_story.img_prompt}
 
 
 @app.route("/theme/<my_theme>")
@@ -60,7 +50,7 @@ def theme(my_theme):
     print("Getting theme: ", my_theme)
     my_story.theme = my_theme
     # store on firebase
-    return {"story": my_theme}
+    return {"theme": my_story.theme}
 
 
 @app.route("/mc_info/<my_mc_info>")
@@ -68,23 +58,24 @@ def mc_info(my_mc_info):
     print("Getting mc_info: ", my_mc_info)
     my_story.mc_info = my_mc_info
     # store on firebase
-    return {"story": my_mc_info}
+    return {"mc_info": my_story.mc_info}
 
 
-@app.route("/story_text")
-def story_text():
+@app.route("/story_text/<my_text>")
+def story_text(my_text):
     # generate plot
     print("Generating story")
     # chat gpt magic
-    text = ""
+    # text = " "
+    my_story.text = my_text
     # store on firebase
-    return {"story_text": text}
+    return {"story_text": my_story.text}
 
 
 @app.route("/view_story")
 def view_story():
     print("Getting story")
-    return {"theme": my_story.theme, "mc_info": my_story.mc_info}
+    return my_story.__dict__
 
 
 if __name__ == "__main__":
