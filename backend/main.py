b6fc0f4365fe33b2373c435e1dfbd7355fc47069
animetibs <101466538+animetibs@users.noreply.github.com>
2023-09-17 10:01:09 -0400

    WE CAN SEE DOGDOG

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ class Story:
 
 app = Flask(__name__)
 CORS(app)
-openai.api_key = "pk-bAAvcNSLkIImdCjCpocEoXswrexCPXVtZdOYWaapPQgtUJsx"
+openai.api_key = "pk-kZNLLuaOkcMCCrqifAxAOFiXFIZtmtmdIhhlUzuTpLzwQaBm"
 # morrigan -> pk-kZNLLuaOkcMCCrqifAxAOFiXFIZtmtmdIhhlUzuTpLzwQaBm
 # sam -> "pk-bAAvcNSLkIImdCjCpocEoXswrexCPXVtZdOYWaapPQgtUJsx"
 openai.api_base = 'https://api.pawan.krd/pai-001-light-beta/v1'
@@ -61,7 +61,7 @@ def summary_gpt(story):
         model="gpt-3.5-turbo",
         prompt="generate a prompt to be a cover image for this story, must be an illustration: " + story,
         temperature=0.7,
-        max_tokens=200,
+        max_tokens=100,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0,
@@ -123,10 +123,7 @@ def story_gen():
     my_story.text = story_gpt(my_story.mc_info, my_story.topic).choices[0].text
     print(my_story.text)
     time.sleep(5)
-    print("Summarizing story: ")
-    my_story.summary = summary_gpt(my_story.text).choices[0].text
-    print(my_story.summary)
-    my_story.img_url = stable_diff(my_story.summary)
+    my_story.img_url = stable_diff(my_story.text)
     print(my_story.img_url)
     # synthesis_input = texttospeech_v1.SynthesisInput(ssml=my_story.text)
     # voice = texttospeech_v1.VoiceSelectionParams(
@@ -145,7 +142,7 @@ def story_gen():
     # my_story.audio = response1.audio_content
     # with open('audiobee.mp3', 'wb', )as output:
     #     output.write(response1.audio_content)
-    return {"story_text": my_story.text, "story_summary": my_story.summary, "image_url": my_story.img_url}
+    return {"story_text": my_story.text, "image_url": my_story.img_url}
 
 
 @app.route("/story_info/")
--- frontend/src/pages/Story.tsx
+++ frontend/src/pages/Story.tsx
@@ -4,7 +4,7 @@ import "./styles.css";
 
 const Story = () => {
   const [storyText, setStoryText] = useState("");
-  const [storySummary, setStorySummary] = useState("");
+  const [imgUrl, setImgUrl] = useState(""); // State to hold the image URL
 
   const generateStory = async () => {
     const response = await fetch("http://127.0.0.1:5000/story_gen/", {
@@ -19,7 +19,7 @@ const Story = () => {
       const data = await response.json();
       console.log(data);
       setStoryText(data.story_text);
-      setStorySummary(data.story_summary);
+      setImgUrl(data.image_url); // Set the image URL
     } else {
       console.error("Failed to generate story");
     }
@@ -30,20 +30,20 @@ const Story = () => {
       <div className="storybody">
         <div className="content">
           <h1 className="header">This is OurBook</h1>
-          <div className="summary-box">
-            <p className="summary-text">{storySummary}</p>
-        </div>
           <button className="button" onClick={generateStory}>
             Generate story
           </button>
           <Link to="/mainhub" className="button">
-          New Story
-        </Link>
+            New Story
+          </Link>
+          <br/>
+          <br/>
+          {imgUrl && <img src={imgUrl} alt="Story Image" style={{ width: '200px', height: 'auto', justifyContent: 'center' }} />}
         </div>
       </div>
-        <div className="storycontent-box">
-          <p className="story-text">{storyText}</p>
-        </div>
+      <div className="storycontent-box">
+        <p className="story-text">{storyText}</p>
+      </div>
     </body>
   );
 };
