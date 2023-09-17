1b2a46d7b915a43359b3d398e4c5178f4766cf87
Sam H <samuelhussain@live.com>
2023-09-16 20:20:48 -0400

    so tired

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,22 @@
 import replicate
-from flask import Flask
+import openai
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 
 class Story:
-    def __init__(self, theme, mc_info, text, img_prompt, img_url):
-        self.theme = theme
+    def __init__(self, topic, mc_info, text, img_prompt, img_url):
+        self.topic = topic
         self.mc_info = mc_info
         self.text = text
         self.img_prompt = img_prompt
         self.img_url = img_url
 
-    def __str__(self, theme, mc_info, text, img_prompt, img_url):
-        return f"Theme: {self.theme}\nMC Info: {self.mc_info}\nText: {self.text}\nImage Prompt: {self.img_prompt}\nImage URL: {self.img_url}"
+    def __str__(self, topic, mc_info, text, img_prompt, img_url):
+        return f"Topic: {self.topic}\nMC Info: {self.mc_info}\nText: {self.text}\nImage Prompt: {self.img_prompt}\nImage URL: {self.img_url}"
 
 
+openai.api_key = "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"
 app = Flask(__name__)
 CORS(app)
 client = replicate.Client(api_token="r8_CLo7yb0uM3cYeHfPs1N7TyKme3Fg4z743YdrG")
@@ -24,7 +26,24 @@ client = replicate.Client(api_token="r8_CLo7yb0uM3cYeHfPs1N7TyKme3Fg4z743YdrG")
 #    input={"prompt": prompt}
 # print(result)
 
-my_story = Story("no theme", "no mc_info", "no text", "no prompt", "no url")
+my_story = Story("no topic", "no mc_info", "no text", "no prompt", "no url")
+
+
+def chat(prompt):
+    return openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=["\n"]
+    )["choices"][0]["text"]
+
+
+# chat_result = chat("Hello world")
+# print(chat_result)
 
 
 def stable_diff(prompt):
@@ -33,8 +52,9 @@ def stable_diff(prompt):
         input={"prompt": prompt}
     )[0]
 
-
 # Generates ai image of scene
+
+
 @app.route("/imagegen/<my_prompt>")
 def imagegen(my_prompt):
     print("Generating image with prompt: ", my_prompt)
@@ -45,12 +65,14 @@ def imagegen(my_prompt):
     return {"image_url": my_story.img_url, "prompt": my_story.img_prompt}
 
 
-@app.route("/theme/<my_theme>")
-def theme(my_theme):
-    print("Getting theme: ", my_theme)
-    my_story.theme = my_theme
+@app.route("/topic/<my_topic>", methods=['GET', 'POST'])
+def topic(my_topic):
+    # post topic recieved to frontend
+
+    print("Getting topic: ", my_topic)
+    my_story.topic = my_topic
     # store on firebase
-    return {"theme": my_story.theme}
+    return {"topic": my_story.topic}
 
 
 @app.route("/mc_info/<my_mc_info>")
@@ -72,9 +94,10 @@ def story_text(my_text):
     return {"story_text": my_story.text}
 
 
-@app.route("/view_story")
-def view_story():
+@app.route("/story_info")
+def story_info():
     print("Getting story")
+    print(my_story)
     return my_story.__dict__
 
 
--- frontend/src/pages/mainhub.tsx
+++ frontend/src/pages/mainhub.tsx
@@ -1,27 +1,57 @@
 // src/pages/MainHub.jsx
+import { useState } from "react";
 import "./styles.css";
 import { Link } from "react-router-dom";
 
 const MainHub = () => {
+  const [userInput, setUserInput] = useState("");
+
+  const handleUserInputChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleTopicSubmit = async () => {
+    console.log(userInput + " submitted");
+    //get request to send form data to backend
+    const response = await fetch("http://127.0.0.1:5000/topic/" + userInput, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: "pls work",
+    });
+    const data = await response.json();
+    console.log(data);    
+  };
+
   return (
     <div>
       {/* Input field for user prompt */}
       <div className="content">
         <h1>
           Learning Topic
-          <hr/>
+          <hr />
         </h1>
         Parents, what are you hoping to teach your little one with this story?
-        <br/>
-        <br/>
+        <br />
+        <br />
         <div>
-        <input className="prompt-input"
-          type="text"
-          placeholder="Learning to share, diversity, "
-        />
+          <input
+            className="prompt-input"
+            type="text"
+            placeholder="Learning to share, diversity, "
+            onChange={handleUserInputChange}
+            value={userInput}
+          />
         </div>
-        <br/>
-        <Link to="/childmode" className="button">
+        <br />
+        <Link
+          to="/childmode"
+          className="button"
+          onClick={async () => {
+            await handleTopicSubmit();
+          }}
+        >
           Submit
         </Link>
       </div>
