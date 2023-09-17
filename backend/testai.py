a11b8634914b2b2f07aa9f7892ceb32cb43f9d43
Sam H <samuelhussain@live.com>
2023-09-16 23:42:33 -0400

    we doin it

--- backend/testai.py
+++ backend/testai.py
@@ -1,20 +1,21 @@
 import openai
 
-openai.api_key = 'pk-IxwZvTOEshpomvdzsHUJZXaADCaVoeUbOhJkMfWjlUEjsoWQ'
+openai.api_key = 'pk-bAAvcNSLkIImdCjCpocEoXswrexCPXVtZdOYWaapPQgtUJsx'
 openai.api_base = 'https://api.pawan.krd/pai-001-light-beta/v1'
 
 character = "theresa the dragon"
 goal = "sharing"
 
 response = openai.Completion.create(
-  model="gpt-3.5-turbo",
-  prompt="write the beginning of a story about a character " + character + "learning about " + goal + "end with a scenario where the character needs to make a decision regarding what they are learning about",
-  temperature=0.7,
-  max_tokens=200,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0,
-  stop=["Human: ", "AI: "]
+    model="gpt-3.5-turbo",
+    prompt="write the beginning of a story about a character " + character + "learning about " + goal +
+    "end with a scenario where the character needs to make a decision regarding what they are learning about",
+    temperature=0.7,
+    max_tokens=200,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=["Human: ", "AI: "]
 )
 
 print(response.choices[0].text)
--- frontend/src/pages/Story.tsx
+++ frontend/src/pages/Story.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import "./styles.css";
 
 const Story: React.FC = () => {
-    return (
-      <div className="body">
-        <div className="content">
-          <h1 className="header">This is OurBook...</h1>
-          <p className="text">
-          </p>
-        </div>
+  return (
+    <div className="body">
+      <div className="content">
+        <h1 className="header">It's storytime mf</h1>
+        <p className="text"></p>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default Story;
+export default Story;
